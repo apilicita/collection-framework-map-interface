41c4afea8842659d6c28e26553b8c860a9a2d072
apilicita <lexgemix@gmail.com>
2026-05-06 13:51:40 +0200

    Antes de que acepta la lista de empleados

--- collection-framework-map-interface/src/main/java/com/example/App.java
+++ collection-framework-map-interface/src/main/java/com/example/App.java
@@ -1,10 +1,15 @@
 package com.example;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class App {
@@ -64,5 +69,153 @@ public class App {
     			collect(Collectors.groupingBy(nombre -> nombre,Collectors.counting()));
     	
     	System.out.println("Mapa resultante:  "+m2);
+    	
+    	/* Creacion de listado de empleado */
+    	
+    	List<Empleado> empleados = new ArrayList<Empleado>();
+    	
+    	Empleado emp1 = Empleado.builder()
+    			.nombre("Jorge Francisco")
+    			.primerApellido("Alborch")
+    			.segundoApellido("Villar")
+    			.genero(Genero.HOMBRE)
+    			.fechaNacimiento(LocalDate.of(1973, Month.JUNE, 23))
+    			.dpto(Dpto.INFORMATICA)
+    			.salario(new BigDecimal(3500.50))
+    			.fechaAlta(LocalDate.of(1985,Month.SEPTEMBER,22))
+    			.build();
+    	
+    	
+    	Empleado emp2 = Empleado.builder()
+    			.nombre("Andres")
+    			.primerApellido("Alonso")
+    			.segundoApellido("Pelaez")
+    			.genero(Genero.HOMBRE)
+    			.fechaNacimiento(LocalDate.of(1983, Month.SEPTEMBER, 23))
+    			.dpto(Dpto.INFORMATICA)
+    			.salario(new BigDecimal(3400.50))
+    			.fechaAlta(LocalDate.of(1995,Month.SEPTEMBER,22))
+    			.build();
+
+    	Empleado emp3 = Empleado.builder()
+    			.nombre("Jeronimo")
+    			.primerApellido("Arenal")
+    			.segundoApellido("Gomez")
+    			.genero(Genero.HOMBRE)
+    			.fechaNacimiento(LocalDate.of(1968, Month.OCTOBER, 20))
+    			.dpto(Dpto.CONTABILIDAD)
+    			.salario(new BigDecimal(3600.50))
+    			.fechaAlta(LocalDate.of(1977,Month.JANUARY,4))
+    			.build();
+    	
+    	Empleado emp4 = Empleado.builder()
+    			.nombre("Carolina")
+    			.primerApellido("Garzon")
+    			.segundoApellido("Becerra")
+    			.genero(Genero.MUJER)
+    			.fechaNacimiento(LocalDate.of(2001, Month.JUNE, 7))
+    			.dpto(Dpto.INFORMATICA)
+    			.salario(new BigDecimal(3700.50))
+    			.fechaAlta(LocalDate.of(2020,Month.SEPTEMBER,22))
+    			.build();
+    	
+    	Empleado emp5 = Empleado.builder()
+    			.nombre("Mariana")
+    			.primerApellido("Garzon")
+    			.segundoApellido("Villar")
+    			.genero(Genero.MUJER)
+    			.fechaNacimiento(LocalDate.of(2000, Month.AUGUST, 4))
+    			.dpto(Dpto.INFORMATICA)
+    			.salario(new BigDecimal(3300.50))
+    			.fechaAlta(LocalDate.of(2022,Month.SEPTEMBER,25))
+    			.build();
+    	
+    	
+    	Empleado emp6 = Empleado.builder()
+    			.nombre("Francisas")
+    			.primerApellido("Alvarez")
+    			.segundoApellido("Gonzalez")
+    			.genero(Genero.MUJER)
+    			.fechaNacimiento(LocalDate.of(1995, Month.DECEMBER, 10))
+    			.dpto(Dpto.RRHH)
+    			.salario(new BigDecimal(2500.50))
+    			.fechaAlta(LocalDate.of(2010,Month.SEPTEMBER,5))
+    			.build();
+    	
+    	Empleado emp7 = Empleado.builder()
+    			.nombre("Maricarmen")
+    			.primerApellido("Becerra")
+    			.segundoApellido("Martinez")
+    			.genero(Genero.MUJER)
+    			.fechaNacimiento(LocalDate.of(2003, Month.FEBRUARY, 23))
+    			.dpto(Dpto.INFORMATICA)
+    			.salario(new BigDecimal(2600.50))
+    			.fechaAlta(LocalDate.of(1990,Month.SEPTEMBER,8))
+    			.build();
+    	
+    	Empleado emp8 = Empleado.builder()
+    			.nombre("Eva")
+    			.primerApellido("Cornide")
+    			.segundoApellido("Machado")
+    			.genero(Genero.MUJER)
+    			.fechaNacimiento(LocalDate.of(1990, Month.MAY, 18))
+    			.dpto(Dpto.INFORMATICA)
+    			.salario(new BigDecimal(3500.50))
+    			.fechaAlta(LocalDate.of(2015,Month.SEPTEMBER,22))
+    			.build();
+    	
+       	Empleado emp9 = Empleado.builder()
+    			.nombre("Alberto")
+    			.primerApellido("Gonzalez")
+    			.segundoApellido("Sanchez")
+    			.genero(Genero.HOMBRE)
+    			.fechaNacimiento(LocalDate.of(1990, Month.FEBRUARY, 14))
+    			.dpto(Dpto.FINANZAS)
+    			.salario(new BigDecimal(2600.50))
+    			.fechaAlta(LocalDate.of(2008,Month.SEPTEMBER,8))
+    			.build();
+    	
+    	Empleado emp10 = Empleado.builder()
+    			.nombre("Javier")
+    			.primerApellido("Gonzalez")
+    			.segundoApellido("Sanchez")
+    			.genero(Genero.HOMBRE)
+    			.fechaNacimiento(LocalDate.of(1995, Month.MAY, 20))
+    			.dpto(Dpto.RRHH)
+    			.salario(new BigDecimal(3500.50))
+    			.fechaAlta(LocalDate.of(2015,Month.SEPTEMBER,22))
+    			.build();
+    	
+    	
+    	empleados = Arrays.asList(emp1,emp2,emp3,emp4,emp5,emp6,emp7,emp8,emp9,emp10);
+    	
+    	/*Crear una coleccion que agrupe empleados por genero*/
+    	//clave: Genero, Valor: Lista Empleados
+    	//Grouping --> agrupe por genero (Hombre y mujer)
+    	//To list ->me los pongo como una lista
+    	
+    	//Map<Genero,List<Empleado>> empleadosPorGenero = empleados.stream().
+    	//		collect(Collectors.groupingBy(empleado-> empleado.getGenero(),
+    	//				Collectors.toList()));
+    
+    	//Map<Genero,List<Empleado>> empleadosPorGenero = empleados.stream().
+    	//		collect(Collectors.groupingBy(Persona::getGenero,
+    	//				Collectors.toList()));
+ 
+    	/* Cuando se recorre una lista que es del mismo tipo que los elementos del valor del mapa, NO HAY que hacer absolutamente nada 
+    	 * para que elemento que circula por la tuberia termine en lista correspondiente al valor de la entrada del mapa
+    	 *
+    	 * Concretamente , en este caso , el Collectors.toList() sobra*/
+    	
+    	Map<Genero,List<Empleado>> empleadosPorGenero = empleados.stream().
+    			collect(Collectors.groupingBy(Persona::getGenero));
+    		
+ 
+ 
+    					
+    	System.out.println("Empleados por Genero:  " +empleadosPorGenero);
+    	
+    	
+    	
     }
 }
